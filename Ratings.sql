f55a80525093ea258c0e87ca261a56f56da9de93
Braden <bradenrkramer@gmail.com>
2021-10-12 19:31:58 -0500

    Update Ratings.sql

--- Ratings.sql
+++ Ratings.sql
@@ -5,14 +5,3 @@ create table ratings (
 	letterboxdurl varchar(50),
 	rating integer
 )
-
-alter table ratings
-alter column title type varchar(125)
-
-
-select count(distinct(title)), title
-from ratings
-group by title
-order by count(distinct(title)) desc
-
-select avg(release_year) from ratings
